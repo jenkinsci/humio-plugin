339ee2c44fe59f9ed3b9ecbf0d5d578911aac0ae
Thomas Anagrius <thb@trifork.com>
2017-09-07 21:00:45 +0200

    Correct help tests for settings

--- src/main/resources/com/humio/jenkinshumio/HumioConfig/config.groovy
+++ src/main/resources/com/humio/jenkinshumio/HumioConfig/config.groovy
@@ -6,24 +6,24 @@ def f = namespace(FormTagLib)
 
 f.section(title: descriptor.displayName) {
     f.entry(field: "serverURL", title: _("Humio Server URL"),
-            help: descriptor.getHelpFile()) {
+            help: _("The URL to the Humio Server. E.g. https://cloud.humio.com/")) {
 
         f.textbox()
     }
 
     f.entry(field: "dataspaceId", title: _("Dataspace ID"),
-            help: descriptor.getHelpFile()) {
+            help: _("The id (name) of the dataspace to store the logs in.")) {
 
         f.textbox()
     }
 
     f.entry(field: "authToken", title: _("Auth Token"),
-            help: descriptor.getHelpFile()) {
+            help: _("The Auth Token found in your Humio Account Page")) {
 
         f.password()
     }
 
-    f.entry(field: "enabled", title: _("Enable Logging")) {
+    f.entry(field: "enabled", title: _("Enable Logging"), help: _("If disabled no logs will be shipped to Humio.")) {
         f.checkbox()
     }
 }
